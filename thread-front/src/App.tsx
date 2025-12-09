e1f8ff7fad19e12989f5d3f8958fb21cb60d41cd
RRFIM <saswswap@gmail.com>
2025-12-09 17:23:28 +0100

    commit for saving

--- thread-front/src/App.tsx
+++ thread-front/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css' 
+import { NavBar } from './components/NavBar.js'
 
 import HomePage from './components/HomePage.jsx'
 
---
+++ thread-front/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import './NavBar.css';
+
+
+export function NavBar()
+{
+    return (
+        <nav className='NavItem'>
+            <a href=""><li><i className="fa-solid fa-circle-plus"></i></li></a>
+            <li><i className="fa-solid fa-user"></i></li> 
+            <li><i className="fa-solid fa-message"></i></li>
+        </nav>
+        
+    )
+}
