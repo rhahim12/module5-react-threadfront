0c3794f7b91c7692688da0ac3e32e755c98518a2
hassani rhahim <rhahim12@gmail.com>
2025-12-09 13:20:37 +0100

    route feed

--- thread-front/src/App.tsx
+++ thread-front/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from './components/HomePage.jsx'
+import Feed from './components/Feed.js'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="feed" element={<Feed />} />
         </Routes>
       </BrowserRouter>
     </>
