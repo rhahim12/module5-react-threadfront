7f4fb818fb0cb1583b56b1e371262f87f38dd15f
RRFIM <saswswap@gmail.com>
2025-12-10 16:49:04 +0100

    NavBar and Profile complete

--- thread-front/src/App.tsx
+++ thread-front/src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { NavBar } from './components/NavBar.js'
 import HomePage from './components/HomePage.jsx'
 import Feed from './components/Feed.js'
-import Profile from './components/Profil.js'
+import Profil from './components/Profil.js'
 import Login from './components/Login.js'
 
 function App() {
@@ -15,10 +16,11 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="feed" element={<Feed />} />
-          <Route path="profil" element={<Profile />} />
+          <Route path="profil" element={<Profil />} />
           <Route path="login" element={<Login />} />
         </Routes>
       </BrowserRouter>
+      <NavBar/>
     </>
   )
 }
