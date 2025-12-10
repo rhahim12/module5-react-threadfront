b3128406a41e6eafe14401668f9a8e58dcd0ca82
Amilcar VILELA <amilcar.vilela@laposte.net>
2025-12-10 10:07:09 +0100

    sauvegarde commentaire

--- thread-front/src/App.tsx
+++ thread-front/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Comment from './components/Comment.js'
 import HomePage from './components/HomePage.jsx'
 
 function App() {
