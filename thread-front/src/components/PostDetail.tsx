f4ecae22ca5f2028ab5afedcafe8f6830eea2d8f
Amilcar VILELA <amilcar.vilela@laposte.net>
2025-12-19 15:42:51 +0100

    commit to save before pull

--- thread-front/src/components/PostDetail.tsx
+++ thread-front/src/components/PostDetail.tsx
@@ -6,90 +6,81 @@ import { PostData } from "../types/PostData.interface.tsx"
 import { CommentsData } from "../types/CommentsData.interface.tsx"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import {Comment} from "./Comment.tsx"
+
 
 
 export function PostDetail() {
-    const [user, setUser] = useState<UserData>()
     const [post, setPost] = useState<PostData>();
     const [comments, setComments] = useState<Array<CommentsData>>([]);
 
     let params = useParams();
 
+    async function fetchComments() {
+        try {
+            if (!params.postId) {
+                throw new Error(`Erreur post indéfini`);
+            }
+            const url = "http://localhost:3000/post/" + params.postId + "/comments";
+            const response = await fetch(url, {
+                method: "GET",
+                credentials: 'include'
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erreur ${response.status}`);
+            }
+            const results: Array<CommentsData> = await response.json();
+            setComments(results);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    async function fetchPost() {
+        try {
+            if (!params.postId) {
+                throw new Error(`Erreur post indéfini`);
+            }
+            const url = "http://localhost:3000/post/" + params.postId;
+            const response = await fetch(url, {
+                method: "GET",
+                credentials: 'include'
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erreur ${response.status}`);
+            }
+            const result: PostData = await response.json();
+            setPost(result);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
     useEffect(() => {
-
-        const fetchUser = async () => {
-            try {
-                const url = "http://localhost:3000/user";
-                const response = await fetch(url, {
-                    method: "GET",
-                    credentials: 'include'
-                });
-
-                if (!response.ok) {
-                    throw new Error(`Erreur ${response.status}`);
-                }
-                const result: UserData = await response.json();
-                setUser(result);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        fetchUser();
-
-
-        const fetchPost = async () => {
-            try {
-                if(!params.postId) {
-                    throw new Error(`Erreur post indéfini`);
-                }
-                const url = "http://localhost:3000/post/" + params.postId;
-                const response = await fetch(url, {
-                    method: "GET",
-                    credentials: 'include'
-                });
-
-                if (!response.ok) {
-                    throw new Error(`Erreur ${response.status}`);
-                }
-                const result: PostData = await response.json();
-                setPost(result);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
         fetchPost();
-
-
-        const fetchComments = async () => {
-            try {
-                if(!params.postId) {
-                    throw new Error(`Erreur post indéfini`);
-                }
-                const url = "http://localhost:3000/post/" + params.postId + "/comments";
-                const response = await fetch(url, {
-                    method: "GET",
-                    credentials: 'include'
-                });
-
-                if (!response.ok) {
-                    throw new Error(`Erreur ${response.status}`);
-                }
-                const results: Array<CommentsData> = await response.json();
-                setComments(results);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        fetchComments();
     }, []);
 
-    return(
-        <div>
+    useEffect(() => {
+        fetchComments();
+    }, [post]);
 
+
+    const commentsElements = comments.map(comment => {
+        return (
+            <Comment commentData = {comment} />
+        )
+    })
+    return (
+        <div>
+            {post && <Post postData = {post}/>}
+            <div className="all-comments">
+            {commentsElements}
+            </div>
         </div>
-    )
+    );
 }
+
 
